67e5c40672b0b7bfc867578bc7364a75e440020d
Sylvia <sylviapap1@gmail.com>
2020-06-27 13:15:16 -0700

    make nav with grid

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -2,16 +2,12 @@ import React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
+  }
 }));
 
 export default function Nav() {
@@ -19,21 +15,38 @@ export default function Nav() {
 
   return (
     <div className={classes.root}>
-      <Link component={RouterLink} to="/" >
-        Home
-      </Link>
-      <Link component={RouterLink} to="/about" >
-        About
-      </Link>
-      <Link component={RouterLink} to="/projects" >
-        Projects
-      </Link>
-      <Link component={RouterLink} to="/resume" >
-        Resume
-      </Link>
-      <Link component={RouterLink} to="/contact" >
-        Contact
-      </Link>      
+      <Grid container spacing={1}>
+        <Grid item xs={2}>
+          <Link component={RouterLink} to="/"
+          variant="body2" color="inherit">
+          Home
+          </Link>
+        </Grid>
+        <Grid item xs={2}>
+          <Link component={RouterLink} to="/about"
+          variant="body2" color="inherit">
+          About
+          </Link>
+        </Grid>
+        <Grid item xs={2}>
+          <Link component={RouterLink} to="/projects"
+          variant="body2" color="inherit">
+          Projects
+          </Link>
+        </Grid>
+        <Grid item xs={2}>
+          <Link component={RouterLink} to="/resume"
+          variant="body2" color="inherit">
+          Resume
+          </Link>
+        </Grid>
+        <Grid item xs={2}>
+          <Link component={RouterLink} to="/contact"
+          variant="body2" color="inherit">
+          Contact
+          </Link> 
+        </Grid>
+      </Grid>     
     </div>
     );
   }
