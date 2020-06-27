f0148deda458c5a7522e5e104e5b4dae8db304bd
Sylvia <sylviapap1@gmail.com>
2020-06-26 18:24:36 -0700

    idk

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,23 +1,10 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
-}));
-
 const Nav = () => {
-  const classes = useStyles();
     return (
-      <nav className={classes.root}>
+      <nav>
         <Grid container spacing={1}>
           <Grid item xs={2.5}>
             <NavLink to="/">Home</NavLink>
