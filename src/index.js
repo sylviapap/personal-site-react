5805a0c6630e8d709634ac1317251c09149449d4
Sylvia <sylviapap1@gmail.com>
2020-06-26 17:01:48 -0700

    removed service worker

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter as Router} from 'react-router-dom';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <Router>
     <App />
   </Router>,
   document.getElementById('root')
-);
-serviceWorker.register();
+)
