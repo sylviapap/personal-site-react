1e97837f1c8530b9bd3f193dd20e099cdedc04df
Sylvia <sylviapap1@gmail.com>
2020-06-28 00:38:56 -0700

    change background color gradient

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,25 +1,58 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {data} from '../services/data'
 
-import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Link from '@material-ui/core/Link';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import Divider from '@material-ui/core/Divider';
+import MailIcon from '@material-ui/icons/Mail';
+import PhoneIcon from '@material-ui/icons/Phone';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
 
-class Contact extends Component {
-  render() {
-    const email = data.email;
-    const phone = data.phone;
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%'  
+  },
+}));
 
-    return (
-      <section id="contact">
-        <Typography variant="h2" component="h2" gutterBottom>
-          Contact Me
-        </Typography>
-        <Typography variant="body1" gutterBottom>
-          {email}<br></br>
-          {phone}
-        </Typography>
-      </section>
-    );
-  }
+export default function Contact() {
+  const classes = useStyles();
+  const email = data.email;
+  const phone = data.phone;
+
+  return (
+    <List className={classes.root}>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar>
+            <MailIcon />
+          </Avatar>
+        </ListItemAvatar>
+        <ListItemText primary={email} />
+      </ListItem>
+      <Divider variant="inset" component="li" />
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar>
+            <PhoneIcon />
+          </Avatar>
+        </ListItemAvatar>
+        <ListItemText primary={phone} />
+      </ListItem>
+      <Divider variant="inset" component="li" />
+      <ListItem>
+        <Link href="https://www.linkedin.com/in/sylvia-pap/">
+        <ListItemAvatar>
+          <Avatar>
+            <LinkedInIcon />
+          </Avatar>
+        </ListItemAvatar>
+        <ListItemText primary="LinkedIn" /></Link>
+      </ListItem>
+    </List>
+  );
 }
-
-export default Contact;
