08440f1c1a4344603d63dc0482082f46bef49d2a
Sylvia <sylviapap1@gmail.com>
2020-06-28 21:05:58 -0700

    remove comments

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,3 @@
-// import React, { Component } from 'react';
-// import Typography from '@material-ui/core/Typography';
-// import {data} from '../services/data'
-
-// class Footer extends Component {
-//   render() {
-//     return (
-//       <Typography variant="caption" display="block" gutterBottom>
-//         <footer>&copy; {data.firstname} {data.lastname} | {new Date().getFullYear()}</footer>
-//       </Typography>
-//     );
-//   }
-// }
-
-// export default Footer;
-
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
