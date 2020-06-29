15f8e9535b41288850b03d4b213ba2d80137fac5
Sylvia <sylviapap1@gmail.com>
2020-06-28 21:58:45 -0700

    grid

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,6 +7,8 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import CardActions from '@material-ui/core/CardActions';
 
 const useStyles = makeStyles({
   root: {
@@ -27,20 +29,42 @@ export default function Home() {
   const classes = useStyles();
 
   return (
-    <Card className={classes.root}>
-      <CardMedia
-        className={classes.media}
-        image={profilepic}
-        title="Contemplative Reptile"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h2" component="h2">
-          Hi, I'm {name}
-        </Typography>
-        <Typography variant="body2" color="textSecondary" component="p">
-          I'm a {description}
-        </Typography>
-      </CardContent>
-    </Card>
+    <Grid container spacing={5}>
+      <Card className={classes.root}>
+        <CardMedia
+          className={classes.media}
+          image={profilepic}
+          title="me"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h2" component="h2">
+            Hi, I'm {name}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            I'm a {description}
+          </Typography>
+        </CardContent>
+      </Card>
+
+      <Card className={classes.root}>
+        <CardContent>
+          <Typography className={classes.title} color="textSecondary" gutterBottom>
+            Technical Skills
+          </Typography>
+          <Typography variant="h5" component="h2">
+            react
+          </Typography>
+          <Typography className={classes.pos} color="textSecondary">
+            ruby
+          </Typography>
+          <Typography variant="body2" component="p">
+            js
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+    </Grid>
   );
 }
