8871e733b34c0784a1040aa073d3be6db566dcce
Sylvia <sylviapap1@gmail.com>
2020-06-28 20:03:48 -0700

    add info

--- src/pages/About.js
+++ src/pages/About.js
@@ -12,7 +12,11 @@ class About extends Component {
         <Typography variant="body1" gutterBottom>
         I am a recent graduate of <Link href="https://flatironschool.com/">Flatiron School</Link>'s Full Stack Immersive Software Engineering <Link href="https://flatironschool.com/career-courses/coding-bootcamp/san-francisco">program</Link>, voted #1 Coding Bootcamp by <Link href="https://www.coursereport.com/best-coding-bootcamps">Course Report</Link>.
         <br></br>
-        When not coding, you can find me with my dogs, eating weird foods, traveling, playing piano, kickboxing, and/or attempting to learn more about coding through <Link href="https://www.instagram.com/p/B-AJtycgYKU/?igshid=12f3s8qalj71s">memes</Link>. 
+        <br></br>
+        In college, I studied philosophy, psychology, economics, and some math. I worked in Washington, D.C. as a paralegal, went to London for a master's degree in International Relations, and then Mexico City to teach. I thought I'd apply to law school, but realized while studying for the LSAT that I missed logic puzzles and proofs, which brought me back to computer science.
+        <br></br>
+        <br></br>
+        When not coding, you can find me with my dogs, traveling, playing piano, kickboxing, and/or attempting to learn about coding through <Link href="https://www.instagram.com/p/B-AJtycgYKU/?igshid=12f3s8qalj71s">memes</Link>. 
         </Typography>
       </section>
     );
