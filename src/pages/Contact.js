c565b07b5de1a6fe064e8092aef56e34861be3bf
Sylvia <sylviapap1@gmail.com>
2020-06-28 23:04:13 -0700

    add grid

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,10 +3,6 @@ import {data} from '../services/data'
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
 import MailIcon from '@material-ui/icons/Mail';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
@@ -14,13 +10,19 @@ import GitHubIcon from '@material-ui/icons/GitHub';
 import ForumIcon from '@material-ui/icons/Forum';
 import TwitterIcon from '@material-ui/icons/Twitter';
 import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     '& > *': {
-      margin: theme.spacing(1),
-      padding: theme.spacing(1)
-    },
+    margin: theme.spacing(1),
+    padding: theme.spacing(1)
+  },
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    color: theme.palette.text.secondary
   },
 }));
 
@@ -30,67 +32,65 @@ export default function Contact() {
 
   return (
     <div className={classes.root}>
-    <Paper elevation={3}>
-    <Typography variant="h2" component="h2" gutterBottom>
-      Contact Me
-    <List className={classes.root}>
-      <ListItem>
-        <ListItemAvatar>
-          <Avatar >
-            <MailIcon/>
-          </Avatar>
-        </ListItemAvatar>
-        <Link href="mailto:sylviapap1@gmail.com" color="inherit">
-          <ListItemText primary={email} />
-        </Link>
-      </ListItem>
-      
-      <ListItem>
-        <ListItemAvatar>
-          <Avatar>
-            <LinkedInIcon />
-          </Avatar>
-        </ListItemAvatar>
-        <Link href={data.linkedin} color="inherit">
-          <ListItemText primary="LinkedIn" />
-        </Link>
-      </ListItem>
-
-      <ListItem>
-        <ListItemAvatar>
-          <Avatar>
-            <GitHubIcon />
-          </Avatar>
-        </ListItemAvatar>
-        <Link href={data.github} color="inherit">
-          <ListItemText primary="GitHub" />
-        </Link>
-      </ListItem>
-
-      <ListItem>
-        <ListItemAvatar>
-          <Avatar>
-            <ForumIcon />
-          </Avatar>
-        </ListItemAvatar>
-        <Link href={data.blog} color="inherit">
-          <ListItemText primary="Blog" />
-        </Link>
-      </ListItem>
-
-      <ListItem>
-        <ListItemAvatar>
-          <Avatar>
-            <TwitterIcon />
-          </Avatar>
-        </ListItemAvatar>
-        <Link href={data.twitter} color="inherit">
-          <ListItemText primary="Twitter" />
-        </Link>
-      </ListItem>
-    </List>
-    </Typography>
-    </Paper>
+      <Grid container spacing={3}>
+        <Grid item xs={11}>
+          <Paper className={classes.paper}>
+            <Typography variant="h2" component="h2" gutterBottom>
+              Contact Me
+            </Typography>
+          </Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>
+            <Link href="mailto:sylviapap1@gmail.com" color="inherit">
+            <Avatar >
+              <MailIcon/>
+            </Avatar>
+            {email}
+            </Link>
+          </Paper>
+        </Grid>
+        <Grid item xs={2}>
+          <Paper className={classes.paper}>
+            <Link href={data.linkedin} color="inherit">
+              <Avatar>
+              <LinkedInIcon /> 
+              </Avatar>
+              LinkedIn
+            </Link>
+          </Paper>
+        </Grid>
+        <Grid item xs={2}>
+          <Paper className={classes.paper}>
+          <Link href={data.github} color="inherit">
+            <Avatar>
+              <GitHubIcon />
+            </Avatar>
+            GitHub
+          </Link>
+        </Paper>
+        </Grid>
+        <Grid item xs={2}>
+          <Paper className={classes.paper}>
+          <Link href={data.blog} color="inherit">
+            <Avatar>
+              <ForumIcon />
+            </Avatar>
+            Blog
+          </Link>
+        </Paper>
+        </Grid>
+        <Grid item xs={2}>
+          <Paper className={classes.paper}>
+          <Link href={data.twitter} color="inherit">
+            <Avatar>
+              <TwitterIcon />
+            </Avatar>
+            Twitter
+          </Link>
+        </Paper>
+        </Grid>
+      </Grid>
     </div>
   );
 }
