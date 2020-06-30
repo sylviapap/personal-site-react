55a55347b516025edc38e2b0fec4d3d42d8c5eb6
Sylvia <sylviapap1@gmail.com>
2020-06-29 23:46:50 -0700

    small formatting changes

--- src/pages/About.js
+++ src/pages/About.js
@@ -33,19 +33,19 @@ export default function About() {
     I am a recent graduate of <Link className={classes.links} underline="none" href="https://flatironschool.com/" >Flatiron School</Link>'s Full Stack Immersive Software Engineering <Link className={classes.links} underline="none" href="https://flatironschool.com/career-courses/coding-bootcamp/san-francisco">program</Link>, voted #1 Coding Bootcamp by <Link className={classes.links} underline="none" href="https://www.coursereport.com/best-coding-bootcamps">Course Report</Link>.
     <br></br>
     <br></br>
-    I graduated from the University of Chicago in 2017, having studied philosophy, psychology, economics, and some math. I worked in Washington, D.C. as a paralegal, went to the London School of Economics for a Master's degree in International Relations, and then Mexico City to teach. 
+    I graduated from the <strong>University of Chicago</strong> in <strong>2017</strong>, having studied philosophy, psychology, economics, and some math. I worked in <strong>Washington, D.C.</strong> as a paralegal, went to the <strong>London School of Economics</strong> for a Master's degree in <strong>International Relations</strong>, and then <strong>Mexico City</strong> to teach. 
     <br></br>
     <br></br>
-    Like many, I can trace my love of coding back to learning HTML as a kid on Neopets and MySpace, but never thought I could make it a career, or felt like I was too far behind.
+    Like many, I can trace my love of coding back to learning <strong>HTML</strong> as a kid on Neopets and MySpace, but never thought I could make it a career.
     <br></br>
     <br></br>
-    But, in late 2019, while studying for the LSAT, I realized I enjoyed logic puzzles and proofs more than law. This brought me back to my long-standing interest in computer science, and as I learned more about coding bootcamps, I started to think maybe it wasn't that unrealistic of a goal.
+    In <strong>2019</strong>, while studying for the LSAT, I realized I enjoyed logic puzzles and proofs more than law. This brought me back to the interest I'd always had in <strong>computer science</strong>, and as I learned more about coding bootcamps, I started to think maybe it wasn't that unrealistic of a switch.
     <br></br>
     <br></br>
-    In January 2020, I moved back to San Francisco to commit to this career change, and have been loving it ever since.
+    In <strong>January 2020</strong>, I moved back to <strong>San Francisco</strong> to commit to this change, and have been loving it ever since.
     <br></br>
     <br></br>
-    Other passions include my dogs, traveling, playing piano, kickboxing, and attempting to learn programming through <Link className={classes.links} underline="none" href="https://www.instagram.com/p/B-AJtycgYKU/?igshid=12f3s8qalj71s">memes</Link>. 
+    Other passions include my dogs, traveling, piano, kickboxing, and attempting to learn programming through <Link className={classes.links} underline="none" href="https://www.instagram.com/p/B-AJtycgYKU/?igshid=12f3s8qalj71s">memes</Link>. 
     </Typography>
     </Paper>
   </div>
