907220a61858331c57a196a8dea563ce170ad8b9
Sylvia <sylviapap1@gmail.com>
2020-07-19 16:47:29 -0700

    Use image for background

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
+import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles({
+  links: {
+    color: "black",
+    borderBottomStyle: "dotted",
+    border: "thin",
+  }
+});
 export default function Footer() {
+  const classes = useStyles();
   return (
     <Typography variant="body2" color="textSecondary">
       {'Copyright © '}
       <Link color="inherit" href="https://sylviapap.com/">
         Sylvia Pap
       </Link>{' '}
-      {new Date().getFullYear()}
+      {new Date().getFullYear()} | Background: <Link href="https://unsplash.com/photos/RsRTIofe0HE" className={classes.links}>Unsplash</Link>
     </Typography>
   );
 }
