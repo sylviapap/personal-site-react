9090738d6cd6b416acae0a196f9261a32a075594
Sylvia <sylviapap1@gmail.com>
2020-07-24 12:11:11 -0700

    Add link/click to images

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
+import CardActions from '@material-ui/core/CardActions';import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Icon from '@material-ui/core/Icon';
+import Link from '@material-ui/core/Link';
 
 const useStyles = makeStyles({
   root: {
@@ -29,13 +30,17 @@ const ProjectCard = props =>  {
 
   return (
     <Card className={classes.root}>
+      <CardActionArea>
+        <Link href={websiteURL ? websiteURL : githubURL}>
         <CardMedia
           component="img"
           alt={description}
           image={img}
           title={title}
           height="250px"
-        />      
+        />
+        </Link>
+      </CardActionArea>
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
             {title}
