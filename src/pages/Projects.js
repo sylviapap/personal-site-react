de768e04ad2ac7e5525812a7d16c02f1ebf6e94e
Sylvia <sylviapap1@gmail.com>
2020-07-24 12:11:36 -0700

    Remove header

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {data} from '../services/data'
 import ProjectCard from '../components/ProjectCard'
-import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 
@@ -18,16 +17,11 @@ export default function Projects() {
   const projects = data.projects;
   const classes = useStyles();
     return (
-      <div>
-      <Typography variant="h2" component="h2" gutterBottom>
-          Projects
-      </Typography>
       <Grid container spacing={1} className={classes.root}>
         {projects.map(project => 
         <Grid item sm={4} key={projects.indexOf(project)}>
           <ProjectCard project={project} />
         </Grid>)}
       </Grid>
-      </div>
   );
 }
