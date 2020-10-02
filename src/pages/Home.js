b704a3d328188491801ecf7439c2778b8c4e3f45
Sylvia <sylviapap1@gmail.com>
2020-10-01 21:24:05 -0700

    Removed unused

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,7 +13,6 @@ import Link from '@material-ui/core/Link';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
-import Divider from '@material-ui/core/Divider';
 
 const useStyles = makeStyles({
   root: {
